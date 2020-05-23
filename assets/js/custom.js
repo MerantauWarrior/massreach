642f54d6891e9613c93a6d0dc667ce2c22e6ae27
MerantauWarrior <boris1488ss@gmail.com>
2020-05-23 06:11:35 +0300

    product page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
-  console.log('hello');
+
+//  Faqs
+  $('.faqs-item__top').click(function () {
+    $(this).parent().toggleClass('faqs-item_opened');
+    // $(this).parent().find('.faqs-item__content').slideToggle(250);
+  });
+//  Header mobile menu
+  $('.header__mobile-menu').click(function () {
+    $('.header-navigation').slideToggle();
+  });
+//  IE9 placeholder
+  $('[placeholder]').focus(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+      input.removeClass('placeholder');
+    }
+  }).blur(function() {
+    var input = $(this);
+    if (input.val() == '' || input.val() == input.attr('placeholder')) {
+      input.addClass('placeholder');
+      input.val(input.attr('placeholder'));
+    }
+  }).blur().parents('form').submit(function() {
+    $(this).find('[placeholder]').each(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+      }
+    })
+  });
+
 });
