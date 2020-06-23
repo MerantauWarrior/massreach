6967cbf1728d51aced6333e20938c8e9b342178a
Boris <boris1488ss@gmail.com>
2020-06-23 21:57:01 +0300

    partner page done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -35,8 +35,44 @@ $(document).ready(function () {
   $('.password-inp__icon').click(function () {
     $(this).parent().toggleClass('password-inp_show');
     var inpType = $(this).siblings('input').prop('type');
-    $(this).siblings('input').prop('type', inpType == 'password' ? 'text' : 'password');
+    $(this).siblings('input').prop('type', inpType === 'password' ? 'text' : 'password');
   });
+//  Partners
+  if($('.partner-feedbacks').length > 0){
+    $('.partner-feedbacks-slider').slick({
+      dots: true,
+      arrows: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  }
 
 //Select
   var x, i, j, selElmnt, a, b, c, img;
